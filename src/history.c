fe1bd91a01829b9ecc592e42c2ef17c75c3ca1d6
freeknx <kclemons96@hotmail.com>
2021-09-28 05:17:31 -0600

    partial update

--- src/history.c
+++ src/history.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-#include <stlib.h>
+#include <stdlib.h>
 #include "history.h"
+#include "tokenizer.h"
 
 /* Initialize the linked list to keep the history. */
 List* init_history(){
@@ -15,24 +16,55 @@ List* init_history(){
 */
 void add_history(List *list, char *str){
   Item *temp = list->root;
-  int index = 1;
-
-  while(temp){
+  Item *node = malloc(sizeof(Item));
+  int length = word_terminator(str)-word_start(str);
+  char  *newString = str;
+  
+  while(temp->next != NULL){
+    //printf("passing %s at [%d]\n", temp->str, temp->id);
     temp = temp->next;
-    index++;
   }
-  temp = malloc(sizeof(Item));
-  temp->id = index;
-  temp->word = str;
+  //printf("inserting %s at [%d]", temp->str, temp->id);
+  
+  node->id = temp->id + 1;
+  node->str = newString;
+  if(temp = list->root){
+    temp->id = temp->id + 1;
+    temp->str = newString;
+  }
+  else
+    temp->next = node;
 }
 
 /* Retrieve the string stored in the node where Item->id == id.
    List* list - the linked list
    int id - the id of the Item to find */
-char *get_history(List *list, int id);
+char *get_history(List *list, int id){
+  Item *temp = list->root;
+  while(temp){
+    if(temp->id == id){
+      return temp->str;
+    }
+    temp = temp->next;
+  }
+}
 
 /*Print the entire contents of the list. */
-void print_history(List *list);
+void print_history(List *list){
+  Item *temp = list->root;
+  while(temp){
+    printf("[%d] %s\n", temp->id, temp->str);
+    temp = temp->next;
+  }
+}
 
 /*Free the history list and the strings it references. */
-void free_history(List *list);
+void free_history(List *list){
+  Item *temp = list->root;
+  Item *nextUp;
+  while(temp){
+    nextUp = temp->next;
+    free(temp);
+    temp = nextUp;
+  }
+}
--- src/uimain.c
+++ src/uimain.c
@@ -10,35 +10,41 @@ int main(){
   char str[50];
   char *strHist;
   int indexHist;
-  int j;
+  int j = 0;
   char temp;
   printf("type something you want to tokenize, or '!n' to display a word on that sentence with 'n' being the index, '!h' to display all words, and '!x' to exit.\n$");
-  fgets(str, 50, stdin);
+  fgets(str, 200, stdin);
   tokens = tokenize(str);
   
   while(1){
+    add_history(history, str);
     if(str[0] == '!'){
       temp = str[1];
-      switch(temp){
-      case 'n':
-	printf("string at index n");
-
-      case 'h':
-	print_tokens(tokens);
-      case 'x':
-	break;
-	
-      default:
-	printf("unknown command: \ncommands are '!n' for word at index 'n', '!h' for history, '!x' to quit. \n$");
-	  fgets(str, 50, stdin);
+      if(temp == 'x'){break;}
+      else if(temp == 'n'){
+	printf("please input desired index: \n$");
+        fgets(str, 200, stdin);
+	j = str[0];
+	printf("j = %d\n", j);
+	if(get_history(history, j) == NULL)
+	  printf("there is nothing at that index\n");
+	else{
+	  printf("[%d] %s\n", j, get_history(history, j));
+	}
       }
-      printf("\n$");
-      fgets(str, 50, stdin);
-      
+      else if(temp == 'h'){print_history(history);}
+      else{
+	printf("unknown command: \ncommands are '!n' for word at index 'n', '!h' for history, '!x' to quit. \n$");
+      }
+    }
+  printf("Enter another string: \n$");
+  fgets(str, 200, stdin);
+  tokens = tokenize(str);
+  print_tokens(tokens);
   }
 }
 
-int menu(char str*){
+/*int menu(char str*){
   
 
-}
+  }*/
